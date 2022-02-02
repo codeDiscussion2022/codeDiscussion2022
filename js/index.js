6999607c15bbfb55555fae356a9ff5551d14c6f7
sohammanjrekar <mrsohammanjrekar@gmail.com>
2022-02-02 11:58:56 +0530

    update files

---
+++ js/index.js
@@ -0,0 +1,40 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Books", "newsletter", "videos", "code"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+});
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function () {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+})
